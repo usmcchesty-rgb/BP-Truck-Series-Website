7d73ebb4748ca1b240b1c74d2f9ab862b74f23bd
Mark Arthur <usmcchesty@gmail.com>
2026-06-07 01:25:07 -0400

    Test direct SimRacerHub standings API

--- api/standings.js
+++ api/standings.js
@@ -1,74 +1,19 @@
-import { fetchHtml, getSettings, getDriverProfiles, slugify } from './_lib.js';
-
-function extractStandingsProps(html) {
-  const marker = 'React.createElement(StandingsTable,';
-  const start = html.indexOf(marker);
-
-  if (start < 0) {
-    throw new Error('Could not find StandingsTable data');
-  }
-
-  const jsonStart = html.indexOf('{', start);
-  if (jsonStart < 0) {
-    throw new Error('Could not find JSON start');
-  }
-
-  let depth = 0;
-  let inString = false;
-  let escape = false;
-
-  for (let i = jsonStart; i < html.length; i++) {
-    const ch = html[i];
-
-    if (escape) {
-      escape = false;
-      continue;
-    }
-
-    if (ch === '\\') {
-      escape = true;
-      continue;
-    }
-
-    if (ch === '"') {
-      inString = !inString;
-      continue;
-    }
-
-    if (!inString) {
-      if (ch === '{') depth++;
-      if (ch === '}') depth--;
-
-      if (depth === 0) {
-        return JSON.parse(html.slice(jsonStart, i + 1));
-      }
-    }
-  }
-
-  throw new Error('Could not find JSON end');
-}
-
 export default async function handler(req, res) {
   try {
-    const settings = await getSettings();
-    const html = await fetchHtml(settings.standingsUrl);
-    const props = extractStandingsProps(html);
+    const response = await fetch(
+      'https://www.simracerhub.com/scoring/get_standings.php?season_id=27987&schedule_id=346493'
+    );
 
-    const profiles = await getDriverProfiles();
-    const bySlug = Object.fromEntries(profiles.map(p => [p.slug, p]));
+    const data = await response.json();
 
-    res.setHeader('Cache-Control', 's-maxage=120, stale-while-revalidate=300');
-    res.status(200).json({
-      settings: {
-        ...settings,
-        playoffCut: props.chase_cutoff || settings.playoffCut
-      },
-      raw: props,
-      schedules: props.schedules || [],
-      rows: [],
-      updatedAt: new Date().toISOString()
+    return res.status(200).json({
+      success: true,
+      keys: Object.keys(data),
+      sampleDrivers: Object.keys(data.rps || {}).slice(0, 10)
     });
   } catch (e) {
-    res.status(500).json({ error: e.message });
+    return res.status(500).json({
+      error: e.message
+    });
   }
 }
