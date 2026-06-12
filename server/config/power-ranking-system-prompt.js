2c00eed1b01fa9895cf7489fe517da328239827b
Mark Arthur <usmcchesty@gmail.com>
2026-06-12 15:19:51 -0400

    Update Power Rankings prompt to v1.1

--- server/config/power-ranking-system-prompt.js
+++ server/config/power-ranking-system-prompt.js
@@ -1,4 +1,4 @@
-export const POWER_RANKING_PROMPT_VERSION = '1.0';
+export const POWER_RANKING_PROMPT_VERSION = '1.1';
 
 export const POWER_RANKING_SYSTEM_PROMPT = `# BP Truck Series Power Rankings System
 
